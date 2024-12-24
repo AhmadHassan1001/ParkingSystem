f0455fcc27c9ae6e397a3782f76a799544ace753
Ahmad Hassan <mi010203269@gmail.com>
2024-12-25 01:42:38 +0200

    initialize

---
+++ backend/src/test/java/com/database/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
