0d52aa0cbc08265ad9c6b3e3fb27f3c635a73943
Ahmad Hassan <mi010203269@gmail.com>
2024-12-27 23:10:17 +0200

    integrated

--- iot_simulator/parking_lot_simulator.py
+++ iot_simulator/parking_lot_simulator.py
@@ -1,18 +1,42 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
 from parking_slot_model import ParkingSlotModel
 from database_connection import DatabaseConnection
+
+
+load_dotenv()
+
+USERNAME = os.getenv("username")
+PASSWORD = os.getenv("password")
+LOT_ID = int(os.getenv("lot_id"))
+
+BASE_URL = "http://localhost:8080"
 
 
 class ParkingLotSimulator:
     def __init__(self, parking_lot_id):
         self.parking_lot_id = parking_lot_id
         self.db = DatabaseConnection()
+        self.token = self.get_token()
         self.parking_slots = self.get_parking_slots()
 
     def get_parking_slots(self):
-        query = "SELECT id FROM parking_spot WHERE parking_lot_id = %s"
-        parking_spot_ids = self.db.query(query, (self.parking_lot_id,))
-        return [ParkingSlotModel(parking_spot_id[0]) for parking_spot_id in parking_spot_ids]
+        response = requests.get(f"{BASE_URL}/parking-lots/{self.parking_lot_id}", headers={"Authorization": f"Bearer {self.token}"})
+        parking_spot_ids = [ spot["id"] for spot in response.json()["parkingSpots"]]
+        print(parking_spot_ids)
+        return [ParkingSlotModel(parking_spot_id, self.token) for parking_spot_id in parking_spot_ids]
     
+    def get_token(self):
+        payload = {
+            "name": USERNAME,
+            "password": PASSWORD
+        }
+        response = requests.post(f"{BASE_URL}/auth/login", json=payload).json()
+        return response["token"]
+
     def run(self):
         for parking_slot in self.parking_slots:
             parking_slot.start()
@@ -22,7 +46,7 @@ class ParkingLotSimulator:
             parking_slot.stop()
 
 if __name__ == "__main__":
-    simulator = ParkingLotSimulator(1)
+    simulator = ParkingLotSimulator(LOT_ID)
     simulator.run()
     input("Press Enter to stop the simulation")
     simulator.stop()
