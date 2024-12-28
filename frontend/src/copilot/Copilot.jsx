4e3d2164b43aedaf1bd4ed3e67d24fb7fdec10f4
Ahmad Hassan <mi010203269@gmail.com>
2024-12-28 06:55:02 +0200

    temp

--- frontend/src/copilot/Copilot.jsx
+++ frontend/src/copilot/Copilot.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styles from "./Copilot.module.css"
 import SmartToyIcon from '@mui/icons-material/SmartToy';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function Copilot() {
   const [question, setQuestion] = useState(null);
@@ -8,9 +9,23 @@ function Copilot() {
   const [showChat, setShowChat] = useState(false);
   const [questionMessage, setQuestionMessage] = useState(null);
 
+  const genAI = new GoogleGenerativeAI("AIzaSyBuGGkUtK3mqCeGBo3STEscCP7Gi-wNT2I");
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const prompt = "Explain how AI works";
+
   const getAnswer = (question) => {
+    setQuestion("");
     setAnswer(()=>"Thinking...");
-    setAnswer(()=>"I am a copilot");
+    model
+      .generateContent(prompt)
+      .then((response) => {
+        console.log(response.candidates[0].content);
+        console.log(response.candidates[0].content.parts[0].text);
+        setAnswer(response.candidates[0].content.parts[0].text);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return (
@@ -22,7 +37,7 @@ function Copilot() {
           <div className={styles.answer}>{answer}</div>
         </div>
         <div className={styles.chatInput}>
-          <input type="text" placeholder="Ask me anything" onChange={(e)=>setQuestion(e.target.value)}/>
+          <input type="text" placeholder="Ask me anything" onChange={(e)=>setQuestion(e.target.value)} value={question}/>
           <button onClick={()=>{getAnswer(); setQuestionMessage(question)}}>Send</button>
         </div>
       </div>)}
