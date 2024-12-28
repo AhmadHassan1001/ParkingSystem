14482b969c1c6d543a998537ff98b92104e468ca
Ahmad Hassan <mi010203269@gmail.com>
2024-12-28 05:12:43 +0200

    temp

--- frontend/src/copilot/Copilot.jsx
+++ frontend/src/copilot/Copilot.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Copilot.module.css"
 import SmartToyIcon from '@mui/icons-material/SmartToy';
 
 function Copilot() {
+  const [question, setQuestion] = useState(null);
+  const [answer, setAnswer] = useState(null);
+  const [showChat, setShowChat] = useState(false);
+
   return (
     <div className={styles.drawer}>
-      <SmartToyIcon />
+      <SmartToyIcon onClick={()=> setShowChat((old)=>!old)}/>
+      {showChat && (<div className={styles.chat}>
+        <div className={styles.chatContent}>
+          <div className={styles.question}>{question}</div>
+          <div className={styles.answer}>{answer}</div>
+        </div>
+        <div className={styles.chatInput}>
+          <input type="text" placeholder="Ask me anything" onChange={(e)=>setQuestion(e.target.value)}/>
+          <button onClick={()=>setAnswer("I am a copilot")}>Send</button>
+        </div>
+      </div>)}
     </div>
   )
 }
